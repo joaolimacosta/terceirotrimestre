a55413d1221cc39ef42f58b33a6ce10cab55047f
joao vitor <108069031+joaolimacosta@users.noreply.github.com>
2023-10-03 09:07:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 alert ('Olá mundo!');
 document.querySelector('.tecla_pom');
+
+function tocaSomPom() {
+  document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom');
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
